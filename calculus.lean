72aedd8f80af0fde9af92d422d1e9613e7c96941
abhimanyupallavisudhir <43954672+abhimanyupallavisudhir@users.noreply.github.com>
2018-12-04 15:45:44 +0000

    Update calculus.lean

--- calculus.lean
+++ calculus.lean
@@ -20,7 +20,7 @@ begin
   apply mul_lt_mul ih (le_of_lt Hxy) Hxpos (le_of_lt (pow_pos (lt_trans Hxpos Hxy) _))
 end
 
-theorem pow_eq {x y : ℝ} {n : ℕ} (Hxpos : 0 < x) (Hypos : 0 < y) (Hnpos : 0 < n) (Hxyn : x ^ n = y ^ n) : x = y :=
+theorem pow_right_inj {x y : ℝ} {n : ℕ} (Hxpos : 0 < x) (Hypos : 0 < y) (Hnpos : 0 < n) (Hxyn : x ^ n = y ^ n) : x = y :=
 begin
   rcases lt_trichotomy x y with hxy | rfl | hyx,
   { exact absurd Hxyn (ne_of_lt (pow_lt hxy Hxpos Hnpos)) },
@@ -28,21 +28,26 @@ begin
   { exact absurd Hxyn (ne_of_gt (pow_lt hyx Hypos Hnpos)) },
 end
 
-noncomputable definition nth_root (x : ℝ) (n : ℕ) : ℝ := real.exp ((real.log x) / n)
+noncomputable definition nth_root (x : ℝ) (n : ℕ) : ℝ := 
+if x = 0 then 0 else exp ((log x) / n)
 
 theorem exp_mul (x : ℝ) : ∀ n : ℕ, real.exp(n*x) = (real.exp(x))^n
 | 0 := by rw [nat.cast_zero, zero_mul, real.exp_zero, pow_zero]
 | (nat.succ n) := by rw [pow_succ', nat.cast_add_one, add_mul, real.exp_add, ←exp_mul, one_mul]
 
-theorem nth_root_pos {x n} : nth_root x n > 0 := real.exp_pos _
+theorem nth_root_pos {x n} : x ≠ 0 → nth_root x n > 0 := by intro; simp [a, nth_root]; apply exp_pos
 
 theorem nth_root_power {x : ℝ} {n} (Hxpos : 0 < x) (Hnpos : 0 < n): (nth_root x n) ^ n = x := 
-by rw [nth_root, ←exp_mul, mul_div_cancel', real.exp_log Hxpos]; rw nat.cast_ne_zero; exact ne_of_gt Hnpos
+begin
+  simp [ne_of_gt Hxpos, nth_root],
+  rw [←exp_mul, mul_div_cancel', exp_log Hxpos], 
+  rw nat.cast_ne_zero, exact ne_of_gt Hnpos
+end
 
-theorem nth_root_unique (x y : ℝ) (n : ℕ)
-  (Hxpos : 0 < x) (Hypos : 0 < y) (Hnpos : 0 < n) (Hynx : y ^ n = x) :
-  y = nth_root x n :=
-pow_eq Hypos nth_root_pos Hnpos (Hynx.trans (nth_root_power Hxpos Hnpos).symm)
+theorem nth_root_unique {x y : ℝ} {n : ℕ}
+(Hxpos : 0 < x) (Hypos : 0 < y) (Hnpos : 0 < n) (Hynx : y ^ n = x) : y = nth_root x n 
+:= pow_right_inj Hypos (nth_root_pos (ne_of_gt Hxpos)) Hnpos (Hynx.trans (nth_root_power Hxpos Hnpos).symm)
+
 -----</DELETE>
 
 
@@ -115,25 +120,9 @@ begin
       change abs (g x / L - 1) < ε,
       rwa [←mul_lt_mul_left (abs_pos_iff.mpr hL), ←abs_mul, mul_sub, mul_div_cancel' _ hL, mul_one] },
     { exact f0 },
-    { rwa div_right_inj hL }},
+    { rwa div_right_inj hL } },
   { exact tendsto_const_nhds }
 end
-
-lemma tendsto_punctured_mul (f : ℝ → ℝ) (g : ℝ → ℝ) (Lf : ℝ) (Lg : ℝ) 
-: tendsto_punctured f Lf → tendsto_punctured g Lg → tendsto_punctured (f * g) (Lf * Lg) :=
-begin
-  intros Hf Hg ε Hε,
-  have Hf1 := Hf (min ε 1) (lt_min Hε (by norm_num : 0 < (1 : ℝ))),
-  have Hg1 := Hg (min ε 1) (lt_min Hε (by norm_num : 0 < (1 : ℝ))),
-  cases Hf1 with δ₁ Hf2, cases Hf2 with Hδ₁ Hf3,
-  cases Hg1 with δ₂ Hg2, cases Hg2 with Hδ₂ Hg3,
-  existsi (min δ₁ δ₂), existsi (lt_min Hδ₁ Hδ₂),
-  intros x hole Hx,
-  have Hf4 := lt_min_iff.mp (Hf3 x hole ((lt_min_iff.mp Hx).1)),
-  have Hg4 := lt_min_iff.mp (Hg3 x hole ((lt_min_iff.mp Hx).2)),
-  sorry
-end
-
 
 lemma tendsto_mul_zero_of_zero (f : ℝ → ℝ) (g : ℝ → ℝ) (L : ℝ)
 : tendsto f (nhds 0) (nhds 0) → tendsto_punctured g L → f 0 = 0 → g 0 = 0 → tendsto (f * g) (nhds 0) (nhds 0) :=
@@ -150,6 +139,108 @@ begin
       { simpa [hx, g0] },
       { have tg4 := tg3 hx hxd, rwa sub_zero at tg4 } } },
   { apply tendsto_mul_zero_of_nonzero, exact h }
+end
+
+lemma tendsto_punctured_of_tendsto (f : ℝ → ℝ) (L : ℝ) : tendsto f (nhds 0) (nhds L) → tendsto_punctured f L :=
+begin
+  intro,
+  simp only [tendsto_nhds_of_metric, dist, sub_zero, tendsto_punctured] at a ⊢,
+  intros e he, have a' := a e he, cases a' with d a'', cases a'' with hd a''',
+  existsi d, existsi hd, intros x hx, exact a''',
+end
+
+lemma tendsto_punctured_neg {f : ℝ → ℝ} {L : ℝ} : tendsto_punctured f L → tendsto_punctured (-f) (-L) :=
+begin
+  intros Hf ε Hε, have Hf1 := Hf ε Hε, cases Hf1 with δ Hf2, cases Hf2 with Hδ Hf3, existsi δ, existsi Hδ,
+  intros x hole Hx, have Hf4 := Hf3 x hole Hx,
+  rwa [pi.neg_apply, ←neg_neg (-f x - -L), neg_neg_sub_neg, abs_neg],
+end
+
+lemma tendsto_punctured_neg_iff (f : ℝ → ℝ) (L : ℝ) : tendsto_punctured f L ↔ tendsto_punctured (-f) (-L) :=
+begin
+  split, {apply tendsto_punctured_neg },
+  intro temp, rw [←neg_neg f, ←neg_neg L],
+  apply tendsto_punctured_neg temp,
+end
+
+private lemma tendsto_punctured_mul_of_pos_nonneg (f : ℝ → ℝ) (g : ℝ → ℝ) {Lf : ℝ} {Lg : ℝ} (hLf : Lf > 0) (hLg : Lg ≥ 0)
+: tendsto_punctured f Lf → tendsto_punctured g Lg → tendsto_punctured (f * g) (Lf * Lg) :=
+begin
+  intros Hf Hg ε Hε, 
+  have hLgf : Lg + Lf > 0 := add_pos_of_nonneg_of_pos hLg hLf,
+  have h : Lg + Lf ≠ 0 := ne_of_gt hLgf,
+  have Hf1 := Hf (min  (min ((ε / 4)/(abs (Lg + Lf))) ((sqrt ε) / 4)) (2 / 3))
+    (lt_min (lt_min (div_pos (div_pos Hε (by norm_num))
+    (abs_pos_of_ne_zero h)) (div_pos (sqrt_pos.mpr Hε) (by norm_num))) (by norm_num)),
+  have Hg1 := Hg (min (min ((ε / 4)/(abs (Lg + Lf))) ((sqrt ε) / 4)) (2 / 3))
+    (lt_min (lt_min (div_pos (div_pos Hε (by norm_num))
+    (abs_pos_of_ne_zero h)) (div_pos (sqrt_pos.mpr Hε) (by norm_num))) (by norm_num)),
+  cases Hf1 with δ₁ Hf2, cases Hf2 with Hδ₁ Hf3,
+  cases Hg1 with δ₂ Hg2, cases Hg2 with Hδ₂ Hg3,
+  existsi (min δ₁ δ₂), existsi lt_min Hδ₁ Hδ₂,
+  intros x hole Hx, rw pi.mul_apply,
+  have Hf4 := lt_min_iff.mp (Hf3 x hole (lt_min_iff.mp Hx).1), rw lt_min_iff at Hf4,
+  have Hg4 := lt_min_iff.mp (Hg3 x hole (lt_min_iff.mp Hx).2), rw lt_min_iff at Hg4,
+  have convertor : f x * g x - Lf * Lg = (f x - Lf) * Lg + (g x - Lg) * Lf + (f x - Lf) * (g x - Lg) := by ring,
+  rw convertor,
+  apply lt_of_le_of_lt (abs_add_three _ _ _ : 
+                      _ ≤  abs ((f x - Lf) * Lg) + abs ((g x - Lg) * Lf) + abs((f x - Lf) * (g x - Lg))),
+  rw [abs_mul, abs_mul, abs_mul],
+  apply lt_of_le_of_lt ( _ : _ ≤ (ε / 4 / abs (Lg + Lf)) * abs Lg + abs (g x - Lg) * abs Lf + abs (f x - Lf) * abs (g x - Lg)),
+  swap,
+    rw [add_le_add_iff_right, add_le_add_iff_right],
+    apply mul_le_mul_of_nonneg_right (le_of_lt Hf4.1.1) (abs_nonneg _),
+  apply lt_of_le_of_lt ( _ : _ ≤ (ε / 4 / abs (Lg + Lf)) * abs Lg + (ε / 4 / abs (Lg + Lf)) * abs Lf + abs (f x - Lf) * abs (g x - Lg)),
+  swap,
+    rw [add_le_add_iff_right, add_le_add_iff_left],
+    apply mul_le_mul_of_nonneg_right (le_of_lt Hg4.1.1) (abs_nonneg _),
+  apply lt_of_le_of_lt (_ : _ ≤ ε / 4 / abs (Lg + Lf) * abs Lg + ε / 4 / abs (Lg + Lf) * abs Lf + ((sqrt ε) / 4) * ((sqrt ε) / 4)),
+  swap,
+    rw add_le_add_iff_left,
+    apply mul_le_mul (le_of_lt Hf4.1.2) (le_of_lt Hg4.1.2) (abs_nonneg _) (le_of_lt (div_pos (sqrt_pos.mpr Hε) (by norm_num))),
+  rw [←mul_add, ←pow_two, div_pow _ (by norm_num : (4 : ℝ) ≠ 0) _, sqr_sqrt (le_of_lt Hε), 
+      abs_of_pos hLf, abs_of_nonneg hLg, abs_of_pos hLgf, div_mul_cancel _ h, 
+      div_eq_mul_one_div, div_eq_mul_one_div ε _, ←mul_add, mul_lt_iff_lt_one_right Hε],
+  norm_num
+end
+
+private lemma tendsto_punctured_mul_of_nonneg_nonneg (f : ℝ → ℝ) (g : ℝ → ℝ) {Lf : ℝ} {Lg : ℝ} (hLf : Lf ≥ 0) (hLg : Lg ≥ 0)
+: tendsto_punctured f Lf → tendsto_punctured g Lg → tendsto_punctured (f * g) (Lf * Lg) :=
+begin
+  cases lt_or_eq_of_le hLf with hLf' hLf',
+  { apply tendsto_punctured_mul_of_pos_nonneg _ _ hLf' hLg },
+  cases lt_or_eq_of_le hLg with hLg' hLg',
+  { intros hf hg, revert hg hf, rw [mul_comm, mul_comm Lf],
+    apply tendsto_punctured_mul_of_pos_nonneg _ _ hLg' hLf },
+  rw [←hLf', ←hLg', zero_mul],
+  intros hf hg ε Hε,
+  have hf1 := hf (sqrt ε) (sqrt_pos.mpr Hε), cases hf1 with δ₁ hf2, cases hf2 with Hδ₁ hf3,
+  have hg1 := hg (sqrt ε) (sqrt_pos.mpr Hε), cases hg1 with δ₂ hg2, cases hg2 with Hδ₂ hg3,
+  existsi (min δ₁ δ₂), existsi lt_min Hδ₁ Hδ₂, intros x hole hx,
+  have hf4 := hf3 x hole (lt_min_iff.mp hx).1,
+  have hg4 := hg3 x hole (lt_min_iff.mp hx).2,
+  rw sub_zero at hf4 hg4 ⊢,
+  rw [pi.mul_apply, abs_mul, ←sqrt_mul_self (le_of_lt Hε), sqrt_mul (le_of_lt Hε)],
+  apply mul_lt_mul' (le_of_lt hf4) hg4 (abs_nonneg _) (sqrt_pos.mpr Hε)
+end
+
+lemma tendsto_punctured_mul (f : ℝ → ℝ) (g : ℝ → ℝ) (Lf : ℝ) (Lg : ℝ)
+: tendsto_punctured f Lf → tendsto_punctured g Lg → tendsto_punctured (f * g) (Lf * Lg) :=
+begin
+  cases lt_or_ge Lf 0 with hLf hLf,
+  { cases lt_or_ge Lg 0 with hLg hLg,
+    { have hLf' := neg_nonneg_of_nonpos (le_of_lt hLf),
+      have hLg' := neg_nonneg_of_nonpos (le_of_lt hLg),
+      rw [tendsto_punctured_neg_iff f, tendsto_punctured_neg_iff g, ←neg_mul_neg, ←neg_mul_neg Lf],
+      apply tendsto_punctured_mul_of_nonneg_nonneg _ _ hLf' hLg' },
+    { have hLf' := neg_nonneg_of_nonpos (le_of_lt hLf),
+      rw [tendsto_punctured_neg_iff f, tendsto_punctured_neg_iff (f * g), neg_mul_eq_neg_mul, neg_mul_eq_neg_mul],
+      apply tendsto_punctured_mul_of_nonneg_nonneg _ _ hLf' hLg } },
+  { cases lt_or_ge Lg 0 with hLg hLg,
+    { have hLg' := neg_nonneg_of_nonpos (le_of_lt hLg),
+      rw [tendsto_punctured_neg_iff g, tendsto_punctured_neg_iff (f * g), neg_mul_eq_mul_neg, neg_mul_eq_mul_neg],
+      apply tendsto_punctured_mul_of_nonneg_nonneg _ _ hLf hLg' },
+    { exact tendsto_punctured_mul_of_nonneg_nonneg _ _ hLf hLg } }
 end
 
 /-must be defined in this odd way because tendsto does not automatically
@@ -259,10 +350,10 @@ begin
   intros n Hn, simp [tendsto_nhds_of_metric, dist],
   intros ε He, existsi (min (1/2 : ℝ) ( (nth_root ε n)/2)), 
   split, 
-  { apply lt_min, norm_num, apply half_pos (exp_pos _) },
+  { apply lt_min, norm_num, apply half_pos (nth_root_pos (ne_of_gt He)) },
   { intros x Hx, rw abs_pow,
     have Hx1 : abs x < 1, apply @lt_trans ℝ _ _ (1/2) _ (lt_min_iff.mp Hx).1, norm_num,
-    have Hx2 : abs x < nth_root ε n := @lt_trans ℝ _ _ ((nth_root ε n)/2) _ (lt_min_iff.mp Hx).2 (half_lt_self nth_root_pos),
+    have Hx2 : abs x < nth_root ε n := @lt_trans ℝ _ _ ((nth_root ε n)/2) _ (lt_min_iff.mp Hx).2 (half_lt_self (nth_root_pos (ne_of_gt He))),
     cases classical.em (x = 0), { rwa [h, abs_zero, zero_pow (nat.pos_of_ne_zero Hn) ] },
     { rw [←@nth_root_power _ n He (nat.pos_iff_ne_zero.mpr Hn)], 
       apply pow_lt Hx2 (abs_pos_iff.mpr h) (nat.pos_of_ne_zero Hn) } }
